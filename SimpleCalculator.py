23e62b7b21d19b311e6f39c2ad5785ddf7904ddf
amogh895 <amoghdixit063@gmail.com>
2025-05-27 13:29:46 +0530

    Add files via upload

---
+++ SimpleCalculator.py
@@ -0,0 +1,47 @@
+
+def add(num1,num2):
+     return num1 + num2
+
+def sub(num1,num2):
+     return num1 - num2 
+
+def multiply(num1,num2):
+     return num1 * num2  
+
+def divide(num1,num2):
+     if num2!=0:
+       return num1 / num2 
+     else:
+          print("Second number is 0.\nPlease select other numbers.")  
+
+print("Welcome "
+      "Please select a operation:\n"
+      "1. Addition\n" 
+      "2. Substraction\n" 
+      "3. Multiplication\n" 
+      "4. Division\n") 
+
+select = int(input("Select a operation from 1,2,3,4:")) 
+
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+
+if select == 1:
+     print(number1, "+", number2, "= ", 
+           add(number1, number2))
+
+elif select == 2:
+     print(number1, "-", number2, "= ", 
+           sub(number1, number2)) 
+     
+elif select == 3:
+     print(number1, "*", number2, "= ", 
+           multiply(number1, number2))
+     
+elif select == 4:
+     print(number1, "/", number2, "= ", 
+           divide(number1, number2))
+
+else:
+     print("Invalid operation! Please select again!")
+     
